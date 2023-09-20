733030b6d08a05a7f69881b4cdf5c59ffc8429cc
Willian Gabriel <willian.minikovski@hotmail.com>
2023-09-20 16:28:40 -0300

    s

---
+++ exercicio.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplacao(numero1, numero2) {
+    return console.log(numero1 * numero2);
+}
+multiplacao(8, 2);
+function dizOla(nome) {
+    return console.log("Olá " + nome);
+}
+dizOla("Willian");
---
+++ exercicio.ts
@@ -0,0 +1,11 @@
+function multiplacao(numero1: number, numero2:number) {
+    return console.log(numero1 * numero2);
+}
+
+multiplacao(8 ,2)
+
+function dizOla(nome: string) {
+    return console.log("Olá " + nome);
+}
+
+dizOla("Willian")
